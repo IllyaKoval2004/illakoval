2f67d5d57c558e69b569018133503e2e7db79dd9
IllyaKoval2004 <103998747+IllyaKoval2004@users.noreply.github.com>
2022-11-05 22:02:16 +0200

    Add files via upload

---
+++ pr4/pr4/js/indv.js
@@ -0,0 +1,106 @@
+const title = document.createElement('h1');
+title.textContent = 'Індивідуальне завдання';
+document.body.insertBefore(title, body.children[0]);
+
+let watchId = null;
+
+let ourCoords = {
+    latitude: 48.94321, 
+    longitude: 24.73380
+};
+
+window.onload=getMyLocation;
+function getMyLocation(){
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+        var watchButton = document.getElementById("watch");
+        watchButton.onclick = watchLocation;
+        var clearWatchButton = document.getElementById("clearWatch");
+        clearWatchButton.onclick = clearWatch;
+    }
+    else{
+        alert("Oops, no geolocation support");
+    }
+}
+function displayLocation (position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let div = document.getElementById("location");
+    div.innerHTML = `You are at Latitude: ${latitude} Longitude: ${longitude}`;
+}
+
+
+function displayError(error) {
+    let errorTypes = {
+        0: "Unknown error",
+        1: "Permission denied by user",
+        2: "Position is not available",
+        3: "Request timed out"
+    };
+    let errorMessage = errorTypes[error.code];
+    if (error.code == 0 || error.code == 2) {
+        errorMessage = errorMessage + " " + error.message;
+    }
+    let div = document.getElementById("location");
+    div.innerHTML = errorMessage;
+}
+
+function computeDistance(startCoords, destCoords) {
+    let startLatRads = degreesToRadians(startCoords.latitude);
+    let startLongRads = degreesToRadians(startCoords.longitude);
+    let destLatRads = degreesToRadians(destCoords.latitude);
+    let destLongRads = degreesToRadians(destCoords.longitude);
+    let Radius = 6371;
+    let distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + Math.cos(startLatRads) * Math.cos(destLatRads) * Math.cos(startLongRads - destLongRads)) * Radius;
+    return distance;
+}
+
+function degreesToRadians(degrees) {
+    let radians = (degrees * Math.PI)/180;
+    return radians;
+}
+
+function displayLocation(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let div = document.getElementById("location");
+    div.innerHTML = `You are at Latitude: ${latitude}, Longitude: ${longitude}`;
+    div.innerHTML += ` (with ${position.coords.accuracy} meters accuracy)`;
+    let km = computeDistance(position.coords, ourCoords);
+    let distance = document.getElementById("distance");
+    distance.innerHTML = `You are ${km} km from the College`; 
+}
+
+function watchLocation() {
+    watchId = navigator.geolocation.watchPosition(displayLocation, displayError);
+}
+
+function clearWatch() {
+    if(watchId) {
+        navigator.geolocation.clearWatch(watchId);
+        watchId = null;
+    }
+}
+
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+marker.bindPopup("string").openPopup();
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
